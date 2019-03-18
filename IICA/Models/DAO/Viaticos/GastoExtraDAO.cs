ae6c92495f067289569bb2f343e91c197144cf0b
pichas <oph.2010.7@gmail.com>
2019-03-18 01:19:00 -0600

    Se agrega dao para gastos extras

---
+++ IICA/Models/DAO/Viaticos/GastoExtraDAO.cs
@@ -0,0 +1,40 @@
+﻿using AccesoDatos;
+using IICA.Models.Entidades;
+using IICA.Models.Entidades.Viaticos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IICA.Models.DAO.Viaticos
+{
+    public class GastoExtraDAO
+    {
+        private DBManager dbManager;
+        public List<GastoExtra> ObtenerTiposGastosExtra()
+        {
+            List<GastoExtra> tiposGastoExtra = new List<GastoExtra>();
+            GastoExtra gastoExtra;
+            try
+            {
+                using (dbManager = new DBManager(Utils.ObtenerConexion()))
+                {
+                    dbManager.Open();
+                    dbManager.ExecuteReader(System.Data.CommandType.StoredProcedure, "DT_SP_OBTENER_GASTO_EXTRA");
+                    while (dbManager.DataReader.Read())
+                    {
+                        gastoExtra = new GastoExtra();
+                        gastoExtra.idGastoExtra = dbManager.DataReader["Id_Gasto_extra"] == DBNull.Value ? 0 : Convert.ToInt32(dbManager.DataReader["Id_Gasto_extra"].ToString());
+                        gastoExtra.descripcion = dbManager.DataReader["descripcion"] == DBNull.Value ? "" : dbManager.DataReader["descripcion"].ToString();
+                        tiposGastoExtra.Add(gastoExtra);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return tiposGastoExtra;
+        }
+    }
+}
