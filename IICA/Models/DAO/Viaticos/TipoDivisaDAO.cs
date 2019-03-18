3bbbc5752c9c97a283720b4e1df024e256e40779
pichas <oph.2010.7@gmail.com>
2019-03-18 00:42:38 -0600

    Se agregan daos de los catalogos

---
+++ IICA/Models/DAO/Viaticos/TipoDivisaDAO.cs
@@ -0,0 +1,40 @@
+﻿using AccesoDatos;
+using IICA.Models.Entidades;
+using IICA.Models.Entidades.Viaticos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IICA.Models.DAO.Viaticos
+{
+    public class TipoDivisaDAO
+    {
+        private DBManager dbManager;
+        public List<TipoDivisa> ObtenerTiposDivisa()
+        {
+            List<TipoDivisa> tiposDivisa = new List<TipoDivisa>();
+            TipoDivisa tipoDivisa;
+            try
+            {
+                using (dbManager = new DBManager(Utils.ObtenerConexion()))
+                {
+                    dbManager.Open();
+                    dbManager.ExecuteReader(System.Data.CommandType.StoredProcedure, "DT_SP_OBTENER_TIPO_DIVISA");
+                    while (dbManager.DataReader.Read())
+                    {
+                        tipoDivisa = new TipoDivisa();
+                        tipoDivisa.idTipoDivisa = dbManager.DataReader["Id_tipo_divisa"] == DBNull.Value ? 0 : Convert.ToInt32(dbManager.DataReader["Id_tipo_divisa"].ToString());
+                        tipoDivisa.descripcion = dbManager.DataReader["descripcion"] == DBNull.Value ? "" : dbManager.DataReader["descripcion"].ToString();
+                        tiposDivisa.Add(tipoDivisa);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return tiposDivisa;
+        }
+    }
+}
---
+++ IICA/Models/DAO/Viaticos/TipoViajeDAO.cs
@@ -0,0 +1,40 @@
+﻿using AccesoDatos;
+using IICA.Models.Entidades;
+using IICA.Models.Entidades.Viaticos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IICA.Models.DAO.Viaticos
+{
+    public class TipoViajeDAO
+    {
+        private DBManager dbManager;
+        public List<TipoViaje> ObtenerTiposViaje()
+        {
+            List<TipoViaje> tiposViaje = new List<TipoViaje>();
+            TipoViaje tipoViaje;
+            try
+            {
+                using (dbManager = new DBManager(Utils.ObtenerConexion()))
+                {
+                    dbManager.Open();
+                    dbManager.ExecuteReader(System.Data.CommandType.StoredProcedure, "DT_SP_OBTENER_TIPO_VIAJE");
+                    while (dbManager.DataReader.Read())
+                    {
+                        tipoViaje = new TipoViaje();
+                        tipoViaje.idTipoViaje = dbManager.DataReader["Id_tipo_viaje"] == DBNull.Value ? 0 : Convert.ToInt32(dbManager.DataReader["Id_tipo_viaje"].ToString());
+                        tipoViaje.descripcion = dbManager.DataReader["descripcion"] == DBNull.Value ? "" : dbManager.DataReader["descripcion"].ToString();
+                        tiposViaje.Add(tipoViaje);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return tiposViaje;
+        }
+    }
+}
