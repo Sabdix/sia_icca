fe0e20cdac2c78b18518882a01348c4e66ec2160
pichas <oph.2010.7@gmail.com>
2019-03-18 03:10:58 -0600

    Se agrega funcion en el controlador para registrar la solicitud del viatico

--- IICA/Controllers/Viaticos/ViaticoController.cs
+++ IICA/Controllers/Viaticos/ViaticoController.cs
@@ -1,4 +1,5 @@
 ﻿using IICA.Models.DAO.Viaticos;
+using IICA.Models.Entidades;
 using IICA.Models.Entidades.Viaticos;
 using System;
 using System.Collections.Generic;
@@ -11,7 +12,7 @@ namespace IICA.Controllers.Viaticos
     public class ViaticoController : Controller
     {
 
-
+        SolicitudViaticoDAO solicitudViaticoDAO;
         // GET: Viatico
         public ActionResult NuevaSolicitud()
         {
@@ -26,6 +27,39 @@ namespace IICA.Controllers.Viaticos
         {
             ViewBag.TiposMediosTrasnporte = new MedioTransporteDAO().ObtenerMediosTransporte().Select(x => new SelectListItem() { Text = x.descripcion, Value = x.idMedioTransporte.ToString() });
             return PartialView(itinerario);
+        }
+
+        [HttpPost, SessionExpire]
+        public ActionResult RegistrarSolicitud(SolicitudViatico solicitudViatico)
+        {
+            try
+            {
+                solicitudViaticoDAO = new SolicitudViaticoDAO();
+                Usuario usuarioSesion = (Usuario)Session["usuarioSesion"];
+                TipoSalida ts = null;
+                ts = new TipoSalida();
+                ts.idTipoSalida = 1;
+                ts.descripcion = "ida";
+                solicitudViatico.itinerario.Add(new Itinerario { origen = "morelia", destino = "leon", fechaSalida = new DateTime(), fechaLLegada = new DateTime(), tipoSalida = ts, linea = "abejita" });
+
+                TipoSalida tsr = null;
+                ts = new TipoSalida();
+                ts.idTipoSalida = 1;
+                ts.descripcion = "regreso";
+                solicitudViatico.itinerario.Add(new Itinerario { origen = "leon", destino = "morelia", fechaSalida = new DateTime(), fechaLLegada = new DateTime(), tipoSalida = tsr, linea = "abejita" });
+                solicitudViatico.estatusSolicitud =EstatusSolicitud.SOLICITUD_ENVIADA;
+                Result result = solicitudViaticoDAO.GuardarSolicitudViatico(solicitudViatico);
+                //if (result.status)
+                //{
+                //    try { Email.NotificacionPermiso((Permiso)result.objeto); }
+                //    catch (Exception ex) { result.mensaje = "Ocurrio un problema al enviar la notificación de correo electronico: " + ex.Message; }
+                //}
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(500, ex.Message);
+            }
         }
     }
 }
--- IICA/Models/DAO/Viaticos/SolicitudViaticoDAO.cs
+++ IICA/Models/DAO/Viaticos/SolicitudViaticoDAO.cs
@@ -65,3 +65,4 @@ namespace IICA.Models.DAO.Viaticos
             }
         }
     }
+}
