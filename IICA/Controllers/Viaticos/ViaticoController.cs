4f8fe27f481abe624238fc893baba50d17abdda4
dsi_edson <edsonpena2121@gmail.com>
2019-03-18 06:57:08 -0600

    Se concluye con el 90% de la solicitud de viatico

--- IICA/Controllers/Viaticos/ViaticoController.cs
+++ IICA/Controllers/Viaticos/ViaticoController.cs
@@ -3,8 +3,10 @@ using IICA.Models.Entidades;
 using IICA.Models.Entidades.Viaticos;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace IICA.Controllers.Viaticos
@@ -28,6 +30,44 @@ namespace IICA.Controllers.Viaticos
         {
             ViewBag.TiposMediosTrasnporte = new MedioTransporteDAO().ObtenerMediosTransporte().Select(x => new SelectListItem() { Text = x.descripcion, Value = x.idMedioTransporte.ToString() });
             return PartialView(itinerario);
+        }
+
+        [HttpPost, SessionExpire]
+        public ActionResult SubirBoletoItinerario()
+        {
+            try
+            {
+                Usuario usuarioSesion = (Usuario)Session["usuarioSesion"];
+                string mensaje=null;
+                string pathFormato = ObtenerBoletoHttpPost(Request,"Boleto_Itinerario", usuarioSesion.emCveEmpleado);
+                if (!string.IsNullOrEmpty(pathFormato))
+                {
+                    mensaje = pathFormato;
+                }
+                return Json(new {mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult EliminarBoletoItinerario(string pathBoleto)
+        {
+            try
+            {
+                pathBoleto = Server.MapPath("~" + pathBoleto);
+                if (System.IO.File.Exists(pathBoleto))
+                {
+                    System.IO.File.Delete(pathBoleto);
+                }
+                return Json(new { data = true }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(500, ex.Message);
+            }
         }
 
         [HttpPost, SessionExpire]
@@ -63,5 +103,42 @@ namespace IICA.Controllers.Viaticos
                 return new HttpStatusCodeResult(500, ex.Message);
             }
         }
+
+
+        #region Funciones - Generales
+        private string ObtenerBoletoHttpPost(HttpRequestBase httpRequestBase, string formato, string usuario)
+        {
+            try
+            {
+                string idAleatorio = Guid.NewGuid().ToString().Substring(0,5)+DateTime.Now.ToString("yyyy_dd_MM_hh_mm");
+                if (httpRequestBase.Files.Count > 0)
+                {
+                    for (int i = 0; i < httpRequestBase.Files.Count; i++)
+                    {
+                        var file = httpRequestBase.Files[i];
+                        if (file != null && file.ContentLength > 0)
+                        {
+                            string pathViaticosItinerariosBoletos = WebConfigurationManager.AppSettings["pathViaticosItinerariosBoletos"].ToString();
+                            //string pathGeneral = pathFormatosIncapacidades + @"\" + usuario + @"\";
+                            string pathGeneral = Server.MapPath("~" + pathViaticosItinerariosBoletos + "/" + usuario + "/");
+                            if (!System.IO.Directory.Exists(pathGeneral))
+                                System.IO.Directory.CreateDirectory(pathGeneral);
+
+                            string nombre = Path.GetFileName(idAleatorio + "_" + formato + "" + Path.GetExtension(file.FileName));
+                            string pathFormato = Path.Combine(pathGeneral, nombre);
+
+                            file.SaveAs(pathFormato);
+                            return pathViaticosItinerariosBoletos + "/" + usuario + "/" + nombre;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return string.Empty;
+        }
+        #endregion Funciones - Generales
     }
 }
