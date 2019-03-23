64438d475ec10b0b3b9f1dc64f1502c81bf7f199
Batman\Christian <perc9004@gmail.com>
2019-03-23 06:10:42 -0600

    mapeo de entidades para los gastos.

---
+++ IICA/Models/Entidades/Viaticos/ComprobacionGasto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IICA.Models.Entidades.Viaticos
+{
+    public class ComprobacionGasto
+    {
+        public int idComprobacionGasto { get; set; }
+        public SolicitudViatico solicitud { get; set; }
+        public string comentario { get; set; }
+        public string pathArchivoXML { get; set; }
+        public string pathArchivoPDF { get; set; }
+        public GastoComprobacion gastoComprobacion { get; set; }
+        public string emisor { get; set; }
+        public double subtotal { get; set; }
+        public double total { get; set; }
+        public string lugar { get; set; }
+
+    }
+}
